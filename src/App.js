217b07dc543206aa8e5cd585f56d5b699706023d
Jaswant Chaudhary <91217491+jchy@users.noreply.github.com>
2021-12-01 21:36:46 +0530

    order button has been created

--- src/App.js
+++ src/App.js
@@ -14,23 +14,65 @@ export default function App() {
     return axios(config);
   };
 
+  const addToCart = (id) => {
+    const payload = {};
+
+    const config = {
+      url: "https://json-server-mocker-masai.herokuapp.com/tasks",
+      method: "post",
+      data: payload
+    };
+    return axios(config);
+  };
+
   useEffect(() => {
     getData().then((data) => {
       data = data.data.cars;
       console.log(data);
       setRes(data);
+      setIsLoading(false);
     });
   }, []);
 
   return (
-    <div className="App">
+    <div className="App" style={{ width: "70%", margin: "auto" }}>
+      <h2>Welcome to Jaswant Car Show-Room</h2>
       {isLoading ? (
         <h3>...Loading</h3>
       ) : (
         res.map((cars) => {
           return (
-            <div>
-              <h3>{cars.name}</h3>
+            <div
+              style={{
+                display: "flex",
+                border: "2px solid white",
+                margin: "20px",
+                borderRadius: "10px"
+                // paddingLeft: "30px"
+              }}
+            >
+              <img src={cars.image} width="300" alt="img" />
+              <div style={{ textAlign: "left", paddingLeft: "30px" }}>
+                <h3>{cars.name}</h3>
+                <h4>Rs. {cars.price}</h4>
+                <p>Type : {cars.type}</p>
+                <p>MFD Year : {cars.year}</p>
+                <button
+                  style={{
+                    background: "black",
+                    color: "white",
+                    borderRadius: "10px",
+                    padding: "5px",
+                    marginBottom: "10px",
+                    width: "100px",
+                    border: "1px solid white"
+                  }}
+                  id={cars.id}
+                  onClick={addToCart}
+                >
+                  Order Now
+                </button>
+              </div>
             </div>
           );
         })
