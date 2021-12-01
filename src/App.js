be1544b46677500ea716f28c5960fe0bbc0148fa
Jaswant Chaudhary <91217491+jchy@users.noreply.github.com>
2021-12-01 20:46:10 +0530

    db.json file has been created

--- src/App.js
+++ src/App.js
@@ -1,10 +1,25 @@
 import "./styles.css";
+import axios from "axios";
+import { useState } from "react";
+
+const getData = () => {
+  const config = {
+    url: "db.json",
+    method: "get"
+  };
+  return axios(config);
+};
+
+getData().then((res) => {
+  console.log(res);
+});
 
 export default function App() {
+  const [loading, setLoading] = useState(true);
   return (
     <div className="App">
-      <h1>Hello CodeSandbox</h1>
-      <h2>Start editing to see some magic happen!</h2>
+      <h2>Welcome to the Cars Point</h2>
+      {loading ? <h3>...Loading</h3> : <h3>cars</h3>}
     </div>
   );
 }
